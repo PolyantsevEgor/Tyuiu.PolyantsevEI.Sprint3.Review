89802f395a23c017316b93411b0bb29d957bb738
Tyuiu.PolyantsevEI@outlook.com <qreft@mail.ru>
2023-11-18 17:15:24 +0500

    Add project files.

---
+++ Tyuiu.PolyantsevEI.Sprint3.TaskReview.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PolyantsevEI.Sprint3.TaskReview.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PolyantsevEI.Sprint3.TaskReview.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f94c442-2801-448a-9d48-d7c2c2fc8b1b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
